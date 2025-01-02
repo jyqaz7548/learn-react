5c87dcdbbdef710ad24dba026fef33ce96167660
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2025-01-02 20:54:37 +0900

    커커커밋

--- my-app/src/chapter03/Library.jsx
+++ my-app/src/chapter03/Library.jsx
@@ -3,12 +3,12 @@ import Book from "./Book";
 
 function Library(props) {
     return (
-        <div>
-            <Book name = "처음 만난 파이썬" numOfPage = {300}/>
-            <Book name = "처음 만난 AWS" numOfPage = {400}/>
-            <Book name = "처음 만난 리액트" numOfPage = {500}/>
-        </div>
+      <div>
+        <Book name="처음 만난 파이썬" numOfPage={300} />
+        <Book name="처음 만난 AWS" numOfPage={400} />
+        <Book name="처음 만난 리액트" numOfPage={500} />
+      </div>
     );
 }
 
-export default Library
+export default Library;
