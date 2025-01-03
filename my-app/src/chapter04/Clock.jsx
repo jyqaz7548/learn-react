4d5053a7197319891f56f06e812af26a809c2ff3
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2025-01-03 19:40:40 +0900

    솔직히 잘 모르겠다

--- my-app/src/chapter04/Clock.jsx
+++ my-app/src/chapter04/Clock.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
 function Clock(props) {
-    return (
-        <div>
-            <h1>안녕, 리액트!</h1>
-            <h2>현재 시간:{new Date().toLocaleTimeString()}</h2>
-        </div>
-    );
+  const [date, setDate] = useState(new Date()); // 현재 시간을 저장할 state
+
+  useEffect(() => {
+    // 1초마다 setDate를 호출하여 state를 업데이트하는 timer 설정
+    const timerID = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    // 컴포넌트가 unmount될 때 timer를 정리하는 cleanup 함수 반환
+    return () => clearInterval(timerID);
+  }, []); // 빈 배열은 컴포넌트가 마운트될 때 한 번만 useEffect가 실행되도록 함
+
+  return (
+    <div>
+      <h1>안녕, 리액트!</h1>
+      <h2>현재 시간: {date.toLocaleTimeString()}</h2>
+    </div>
+  );
 }
 
 export default Clock;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './index.css';  // 필요한 경우 import
+import Clock from './chapter04/Clock'; // Clock 컴포넌트 import
 import reportWebVitals from './reportWebVitals';
 
-import Library from './chapter03/Library';
-import Clock from './chapter04/Clock';
 
-setInterval(() => {
-  ReactDOM.render(
-      <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render( // Clock 컴포넌트 한 번만 렌더링
+  <React.StrictMode>
     <Clock />
-  </React.StrictMode>,
-  document.getElementById('root')
-  )
-}, 1000);
+  </React.StrictMode>
+);
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(); // 웹 vitals 측정
+
+// 사용하지 않는 import문 제거 (App, Library)
