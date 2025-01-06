906a0ef238f1cdc546d129b6bf298e33cacdd92e
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2025-01-06 19:56:39 +0900

    Update CommentList.jsx

--- my-app/src/chapter05/CommentList.jsx
+++ my-app/src/chapter05/CommentList.jsx
@@ -1,2 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Comment from './Comment';
+
+function CommentList(props){
+    return (
+        <div>
+            <Comment/>
+        </div>
+    );
+}
+
+export default CommentList;
