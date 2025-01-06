d0121b2a89a3e4884ec673992a25d80c6379c012
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2025-01-06 20:16:50 +0900

    알다가도 모르겠는 리액트

--- my-app/src/chapter05/CommentList.jsx
+++ my-app/src/chapter05/CommentList.jsx
@@ -1,10 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import Comment from './Comment';
 
+
+const comments = [
+    {
+        name:"유준영",
+        comment:"앙 기모딱따구리",
+    },
+    {
+        name:"크크루삥뽕",
+        comment:"이이이이잉 기뮤링",
+    },
+    {
+        name:"으하하",
+        comment:"으하하하하하하하하",
+    },
+]
+
+
+
+
 function CommentList(props){
     return (
         <div>
-            <Comment/>
+            {comments.map((comment) => {
+                return (
+                    <Comment name={comment.name} comment={comment.comment}/>
+                );
+            })}
         </div>
     );
 }
