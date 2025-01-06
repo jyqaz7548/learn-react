40b0f521942d2ea1e2357cedf9f0d7d688e303da
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2025-01-06 19:55:40 +0900

    댓글

--- my-app/src/chapter05/Comment.jsx
+++ my-app/src/chapter05/Comment.jsx
@@ -1,1 +1,11 @@
 import React, { useState, useEffect } from 'react';
+
+function Comment(props) {
+    return (
+        <div>
+            <h1>첫 컴포넌트를 만들었다!</h1>
+        </div>
+    );
+}
+
+export default Comment;
