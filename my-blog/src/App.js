e22841f707ebb35a02c8d88ba51f3d4eba8bc70a
jyqaz7548 <165270263+jyqaz7548@users.noreply.github.com>
2025-04-16 21:52:30 +0900

    블로그

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import basic_profile from './assets/basic_profile.png';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <div className='menu'>
+      <h1 className='title'>고양이 블로그</h1>
+      <img src={basic_profile} alt="12" className='basic_profile_img' />
+      <div className='line'></div>
+      <span className='welcome'>안녕하세요,<br/> 고양이 블로그입니다.</span>
     </div>
+  </div>
   );
 }
-
 export default App;
